7303973a998b40408581f8b7f10895287c1862b9
J.D. Lindsay <kraahkan@gmail.com>
2026-03-09 13:14:20 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+window.DelveConfig = {
+  GAME_CONFIG: { 
+    uniqueCharLimit: 3,
+    animDurationMs: 300,
+    startingStats: { fear: 50, horde: 50, might: 50, loot: 50 },
+    statCriticalThresholds: { low: 15, high: 85 }
+  },
+  RULER_NAMES: ["Goblin King", "The Great Boss", "Overlord of the Depths"],
+  RULER_FLAVORS: ["Ruler of the dark below.", "A tyrant of the deepest levels.", "A malevolent force of management."]
+};
+
